535f82ee8729ad62abb27f53954a686ddcd2e45e
KarteekReddy1 <104560543+KarteekReddy1@users.noreply.github.com>
2026-03-13 14:27:17 +0530

    Add DynamoDB table for state locking and S3 backend
    Added a DynamoDB table for state locking and configured S3 backend.

--- prod/S3_Bucket/main.tf
+++ prod/S3_Bucket/main.tf
@@ -33,3 +33,41 @@ resource "aws_s3_bucket_versioning" "bucket2_versioning" {
     status = "Enabled"
   }
 }
+
+
+#Dynamodc_table for state locking
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "dev"
+  }
+}
+#backend code:
+
+terraform {
+  backend "s3" {
+    bucket         = "aluruarumullaa1"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+
+
+
+
+
+
+
+
